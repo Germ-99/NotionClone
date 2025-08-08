7d455b0436e403ea775db08d46540c7f4bb56cf2
CsharpProgramming <chashtag.code@gmail.com>
2025-08-08 15:59:35 +0200

    Super basic UI concept.

--- NotionClone/Form1.Designer.cs
+++ NotionClone/Form1.Designer.cs
@@ -28,34 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.ButtonClose = new FontAwesome.Sharp.IconPictureBox();
+            this.ButtonMax = new FontAwesome.Sharp.IconPictureBox();
+            this.ButtonMin = new FontAwesome.Sharp.IconPictureBox();
+            this.SidePanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonMin)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // SidePanel
             // 
-            this.panel1.Location = new System.Drawing.Point(-1, -2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(239, 770);
-            this.panel1.TabIndex = 0;
+            this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.SidePanel.Controls.Add(this.label1);
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 0);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(223, 805);
+            this.SidePanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Variable Display", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User\'s Notion";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Variable Text", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.richTextBox1.Location = new System.Drawing.Point(267, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(978, 98);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Title";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Font = new System.Drawing.Font("Segoe UI Variable Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.richTextBox2.Location = new System.Drawing.Point(267, 169);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(978, 534);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.Controls.Add(this.ButtonMin);
+            this.TopPanel.Controls.Add(this.ButtonMax);
+            this.TopPanel.Controls.Add(this.ButtonClose);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(223, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1080, 35);
+            this.TopPanel.TabIndex = 6;
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ButtonClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonClose.IconChar = FontAwesome.Sharp.IconChar.X;
+            this.ButtonClose.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonClose.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            this.ButtonClose.IconSize = 20;
+            this.ButtonClose.Location = new System.Drawing.Point(1060, 0);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Padding = new System.Windows.Forms.Padding(0, 7, 0, 0);
+            this.ButtonClose.Size = new System.Drawing.Size(20, 35);
+            this.ButtonClose.TabIndex = 12;
+            this.ButtonClose.TabStop = false;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // ButtonMax
+            // 
+            this.ButtonMax.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ButtonMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonMax.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonMax.IconChar = FontAwesome.Sharp.IconChar.Square;
+            this.ButtonMax.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonMax.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            this.ButtonMax.IconSize = 20;
+            this.ButtonMax.Location = new System.Drawing.Point(1040, 0);
+            this.ButtonMax.Name = "ButtonMax";
+            this.ButtonMax.Padding = new System.Windows.Forms.Padding(0, 7, 0, 0);
+            this.ButtonMax.Size = new System.Drawing.Size(20, 35);
+            this.ButtonMax.TabIndex = 14;
+            this.ButtonMax.TabStop = false;
+            this.ButtonMax.Click += new System.EventHandler(this.ButtonMax_Click);
+            // 
+            // ButtonMin
+            // 
+            this.ButtonMin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ButtonMin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.ButtonMin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonMin.IconChar = FontAwesome.Sharp.IconChar.Minus;
+            this.ButtonMin.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+            this.ButtonMin.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.ButtonMin.IconSize = 20;
+            this.ButtonMin.Location = new System.Drawing.Point(1020, 0);
+            this.ButtonMin.Name = "ButtonMin";
+            this.ButtonMin.Padding = new System.Windows.Forms.Padding(0, 7, 0, 0);
+            this.ButtonMin.Size = new System.Drawing.Size(20, 35);
+            this.ButtonMin.TabIndex = 15;
+            this.ButtonMin.TabStop = false;
+            this.ButtonMin.Click += new System.EventHandler(this.ButtonMin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1287, 766);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(1303, 805);
+            this.Controls.Add(this.TopPanel);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.SidePanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximumSize = new System.Drawing.Size(1920, 1080);
             this.MinimumSize = new System.Drawing.Size(1303, 805);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Notion Clone";
+            this.SidePanel.ResumeLayout(false);
+            this.SidePanel.PerformLayout();
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ButtonMin)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Panel TopPanel;
+        private FontAwesome.Sharp.IconPictureBox ButtonMin;
+        private FontAwesome.Sharp.IconPictureBox ButtonMax;
+        private FontAwesome.Sharp.IconPictureBox ButtonClose;
     }
 }
 
--- NotionClone/Form1.cs
+++ NotionClone/Form1.cs
@@ -6,15 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 using System.Windows.Forms;
 
 namespace NotionClone
 {
     public partial class Form1 : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
         public Form1()
         {
             InitializeComponent();
+            TopPanel.MouseDown += TopPanel_MouseDown;
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void ButtonMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonMax_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
